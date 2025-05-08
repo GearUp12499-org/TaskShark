473073f415a0bc5191d44276db27e7c7c4d1888a
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2025-05-08 14:29:41 -0700

    update logging to support deferred formatting

--- taskshark/src/main/java/io/github/gearup12499/taskshark/api/LogOutlet.kt
+++ taskshark/src/main/java/io/github/gearup12499/taskshark/api/LogOutlet.kt
@@ -1,5 +1,7 @@
 package io.github.gearup12499.taskshark.api
 
+import org.intellij.lang.annotations.PrintFormat
+
 abstract class LogOutlet {
     companion object {
         @JvmStatic var currentLogger: LogOutlet = DefaultLogOutlet()
@@ -17,18 +19,39 @@ abstract class LogOutlet {
     abstract var level: Level
     protected abstract fun internalLog(message: String?, ex: Throwable?, level: Level)
 
-    fun log(message: String?, ex: Throwable?, level: Level) {
+    fun escape(rawMessage: String) = rawMessage.replace("%", "%%")
+
+    fun log(level: Level, ex: Throwable?, @PrintFormat fmt: String?, vararg parameters: Any?) {
         if (level.value < this.level.value) return
-        internalLog(message, ex, level)
+        internalLog(fmt?.format(*parameters), ex, level)
     }
 
-    open fun trace(message: String) = log(message, null, Level.Trace)
-    open fun debug(message: String) = log(message, null, Level.Debug)
-    open fun info(message: String) = log(message, null, Level.Info)
-    open fun warn(message: String) = log(message, null, Level.Warn)
-    open fun error(message: String) = log(message, null, Level.Error)
-    open fun error(message: String, ex: Throwable) = log(message, ex, Level.Error)
-    open fun error(ex: Throwable) = log(null, ex, Level.Error)
+    open fun trace(message: String)
+        = log(Level.Trace, null, escape(message))
+    open fun trace(@PrintFormat fmt: String, first: Any?, vararg rest: Any?)
+        = log(Level.Trace, null, fmt, *rest)
+    open fun debug(message: String)
+        = log(Level.Debug, null, escape(message))
+    open fun debug(@PrintFormat fmt: String, first: Any?, vararg rest: Any?)
+        = log(Level.Debug, null, fmt, *rest)
+    open fun info(message: String)
+        = log(Level.Info, null, escape(message))
+    open fun info(@PrintFormat fmt: String, first: Any?, vararg rest: Any?)
+        = log(Level.Info, null, fmt, *rest)
+    open fun warn(message: String)
+        = log(Level.Warn, null, escape(message))
+    open fun warn(@PrintFormat fmt: String, first: Any?, vararg rest: Any?)
+        = log(Level.Warn, null, fmt, *rest)
+    open fun error(message: String)
+        = log(Level.Error, null, escape(message))
+    open fun error(@PrintFormat fmt: String, first: Any?, vararg rest: Any?)
+        = log(Level.Error, null, fmt, *rest)
+    open fun error(ex: Throwable, message: String)
+        = log(Level.Error, ex, escape(message))
+    open fun error(ex: Throwable, @PrintFormat fmt: String, first: Any?, vararg rest: Any?)
+        = log(Level.Error, ex, fmt, *rest)
+    open fun error(ex: Throwable)
+        = log(Level.Error, ex, null)
 
     class DefaultLogOutlet : LogOutlet() {
         override var level = Level.Nothing
