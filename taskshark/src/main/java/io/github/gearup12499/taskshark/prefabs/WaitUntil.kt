7fd5bfd8291c31117a087142934cb382b7540228
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2025-08-09 20:45:24 -0700

    new utility WaitUntil task

---
+++ taskshark/src/main/java/io/github/gearup12499/taskshark/prefabs/WaitUntil.kt
@@ -0,0 +1,14 @@
+package io.github.gearup12499.taskshark.prefabs
+
+import io.github.gearup12499.taskshark.Task
+
+/**
+ * Simple task that repeatedly checks if the [condition] returns `true`.
+ */
+class WaitUntil(val condition: Condition) : Task() {
+    override fun onTick(): Boolean = condition.check()
+
+    fun interface Condition {
+        fun check(): Boolean
+    }
+}
